eab79aedfbec6b7cd14a8653dfb1a25af1611a6b
ComradeFlammpanzer <flmpz@yandex.ru>
2019-07-17 22:01:40 +0300

    Update userauth.py

--- userauth.py
+++ userauth.py
@@ -4,8 +4,8 @@ import sys
 userpassfile="/tmp/user.pass"
 
 if len(sys.argv)!=3:
-        print("Return 0 errorcode, if auth success, else 1")
-        print("usage: %s user password" % sys.argv[0])
+        print "Return 0 errorcode, if auth success, else 1"
+        print "usage: %s user password" % sys.argv[0]
         sys.exit(3)
 
 givenuser,givenpassword=sys.argv[1],sys.argv[2]
@@ -17,12 +17,12 @@ for userpass in userpassfileD:
         if givenuser==username:
                 import bcrypt
                 if bcrypt.hashpw(givenpassword, password) == password.rstrip():
-                        print("Password correct.")
+                        print "Password correct."
                         sys.exit(0)
                 else:
-                        print("Password INcorrect.")
+                        print "Password INcorrect."
                         sys.exit(1)
                 break
 
-print("Password INcorrect.")
+print "Password INcorrect."
 sys.exit(1)
